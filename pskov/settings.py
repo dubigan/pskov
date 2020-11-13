f1aa06a5a14aceea65e4d4e6a6ce89d701b80016
ioak <353991@gmail.com>
2020-11-13 08:10:04 +0300

    new frontend

--- pskov/settings.py
+++ pskov/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'testforjob.apps.TestforjobConfig',
-    'frontend.apps.FrontendConfig'
+    'frontend'
 ]
 
 MIDDLEWARE = [
