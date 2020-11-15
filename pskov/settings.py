fa5b0b892ebc0d7f67ab47e256d4255221315c74
ioak <353991@gmail.com>
2020-11-15 19:13:26 +0300

    postgre

--- pskov/settings.py
+++ pskov/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'pskov.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd4k293dv5ufomj',
+        'USER' : 'ebdnwolckjpnqu',
+        'PASSWORD' : '7dc6fc0635323d2ae7ca523c3b13c0e14b28e0953ea11be2e2b0691390759568',
+        'HOST' : 'ec2-52-31-233-101.eu-west-1.compute.amazonaws.com',
+        'PORT' : '5432',        
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
