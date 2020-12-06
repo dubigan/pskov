992c1ea96fffff67891f4d84f454dbbc81d8bfc6
oak <oak@localhost.localdomain>
2020-12-07 00:27:20 +0300

    settings

--- pskov/settings.py
+++ pskov/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ol9jrwu&frg$819vp%k$rf0o!3h+r($d=jao96x5kp0m=p@a#y'
+#SECRET_KEY = 'ol9jrwu&frg$819vp%k$rf0o!3h+r($d=jao96x5kp0m=p@a#y'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
