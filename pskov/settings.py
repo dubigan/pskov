c1636ef71b15e45c5e0b5bc399facfb280f1179e
oak <oak@localhost.localdomain>
2020-12-07 01:10:51 +0300

    requirements

--- pskov/settings.py
+++ pskov/settings.py
@@ -33,16 +33,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes',
-    #'django.contrib.sessions',
-    #'django.contrib.messages',
-    #'django.contrib.staticfiles',
-    #'rest_framework',
-    #'channels',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'channels',
     'testforjob.apps.TestforjobConfig',
-    #'frontend.apps.FrontendConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
