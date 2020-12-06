343dbaf5644f9de44729eaeac9058fc29ed5fcd5
oak <oak@localhost.localdomain>
2020-12-06 22:43:09 +0300

    settings

--- pskov/settings.py
+++ pskov/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'testforjob.apps.TestforjobConfig',
+    'testforjob',
     'frontend.apps.FrontendConfig',
     'channels'
 ]
